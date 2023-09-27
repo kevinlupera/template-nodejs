297d25ed699a2e71c7424d921e139b5fef08c7eb
kevin-lupera <kevinlupera@gmail.com>
2023-09-26 21:56:01 -0500

    finish apis

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 const port = process.env.PORT ?? 3000;
-
+const db = require('./queries')
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+  
 app.use(express.static('public'))
 
-app.get('*', (req, res) => {
-    res.redirect('/');
-})
+app.get('/events/:page/:idCategory', async (req, res) => {
+    await db.getEvents(req, res);
+  });
+
+app.get('/categories', db.getCategories);
+
+// app.get('*', (req, res) => {
+//     res.redirect('/');
+// })
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
---
+++ queries.js
@@ -0,0 +1,64 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: "fl0user",
+  host: "ep-soft-salad-49680337.us-east-2.aws.neon.tech",
+  database: "teveplaydb",
+  password: "U29ntQhXCKgd",
+  port: 5432,
+  ssl: {},
+});
+
+const ROWS_BY_PAGE = 10;
+
+async function getTotalEvents(idCategory) {
+  let count = 0;
+  let response;
+  try {
+    response = await pool.query(`SELECT count(id) FROM events where id_category = ${idCategory}`);
+  } catch (error) {
+    throw error;
+  }
+  return response ? response.rows[0].count : count;
+}
+
+const getEvents = async (request, response) => {
+  const page = parseInt(request.params.page);
+  const idCategory = parseInt(request.params.idCategory);
+  const offset = page == 1 ? 0 : ROWS_BY_PAGE * page;
+  const total = parseInt(await getTotalEvents(idCategory));
+  const totalPages = total ? parseInt(total / ROWS_BY_PAGE) + 1 : 0;
+  pool.query(
+    `SELECT * FROM events where id_category = ${idCategory} ORDER BY id ASC LIMIT ${ROWS_BY_PAGE} OFFSET ${offset}`,
+    (error, resutls) => {
+      if (error) {
+        throw error;
+      }
+      response
+        .status(200)
+        .json(formatResponse(resutls.rows, page, totalPages, total));
+    }
+  );
+};
+
+const getCategories = (request, response) => {
+  pool.query("SELECT * FROM categories ORDER BY id ASC", (error, resutls) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(formatResponse(resutls.rows));
+  });
+};
+const formatResponse = (rows, page, totalPages, total) => {
+  return {
+    page,
+    results: rows,
+    total_pages: totalPages,
+    total_results: total,
+  };
+};
+
+module.exports = {
+  getCategories,
+  getEvents,
+};
